42209c2a5dd9b78ae6f68346c8f289f826084515
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-20 18:25:27 +0100

    fixed typo

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
-  if (node.internal.type !== `MarkdownRemark`) {
+  if (node.internal.type === `MarkdownRemark`) {
     const slug = createFilePath({ node, getNode })
 
     createNodeField({
