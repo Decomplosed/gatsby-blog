463e87e3fe2dbc886cae497e0d07c0463ec617d7
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-20 17:41:46 +0100

    added totalCount to the query

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,17 @@ import SEO from '../components/seo'
 export default ({ data }) => (
   <Layout>
     <SEO title='Home' />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <span>Bart's Thoughts</span>
     </div>
-    <Link to='/page-2/'>Go to page 2</Link>
+
   </Layout>
 )
 
 export const query = graphql`
   query {
     allMarkdownRemark {
+      totalCount
       edges {
         node {
           frontmatter {
