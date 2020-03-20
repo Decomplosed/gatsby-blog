6eba8dc5258f51f2e175f7578abd0254229ad3a8
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-20 18:40:50 +0100

    wrote a query to grab slug related data

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,3 +1,16 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+
+export default ({ data }) => {}
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark( fields: { slug: { eq: $slug }}) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
