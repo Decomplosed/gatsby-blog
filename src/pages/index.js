49f9ee9adb7ba0743d06016d95921a6e9c06f9f6
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-20 17:46:08 +0100

    printed out the title and date of each ndoe

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,13 @@ export default ({ data }) => (
     <div>
       <h1>Bart's Thoughts</h1>
       <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <span>
+            {node.frontmatter.title} - {node.frontmatter.date}
+          </span>
+        </div>
+      ))}
     </div>
   </Layout>
 )
