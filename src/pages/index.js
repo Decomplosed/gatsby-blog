2749f1457b9c2eb24e570c81af709f5d45c054b8
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-21 07:26:24 +0100

    import in Link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import styled from 'styled-components'
 
 import Layout from '../components/layout'
